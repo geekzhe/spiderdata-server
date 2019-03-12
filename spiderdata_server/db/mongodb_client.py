16e3aa200f4b6c771d7da5ff353445714cc3444a
mz <mzcwlong@163.com>
2019-03-12 13:35:00 +0800

    设计数据库类框架
    Signed-off-by: mz <mzcwlong@163.com>

--- spiderdata_server/db/mongodb_client.py
+++ spiderdata_server/db/mongodb_client.py
@@ -1,3 +1,30 @@
-class MongodbClient(object):
+class MongodbBase(object):
+    """数据库操作基本类，包含与业务无关的数据库操作方法"""
     def __init__(self):
         pass
+
+    def conn(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    def select(self):
+        pass
+
+    # TODO: 可以加一些聚合操作等方法
+
+
+class MongodbClient(MongodbBase):
+    """与业务有关的数据库操作方法类"""
+    def __init__(self):
+        # TODO: 从配置文件读取数据库连接信息
+        super().__init__()
+
+    # TODO: 这里写与业务有关的数据库操作方法
--- spiderdata_server/db/mysql_client.py
+++ spiderdata_server/db/mysql_client.py
@@ -1,3 +1,35 @@
-class MysqlClient(object):
+class MysqlBase(object):
+    """数据库操作基本类，包含与业务无关的数据库操作方法"""
     def __init__(self):
+        pass
+
+    def conn(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    def select(self):
+        pass
+
+
+class MysqlClient(MysqlBase):
+    """与业务有关的数据库操作方法类"""
+    def __init__(self):
+        # TODO: 从配置文件读取数据库连接信息
+        super().__init__()
+
+    def add_user(self):
+        pass
+
+    def update_user(self):
+        pass
+
+    def get_user(self):
         pass
